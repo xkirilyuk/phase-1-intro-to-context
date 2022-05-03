62f7ba2f0aacf161c246020a67b36f7826cbac59
Xenia <kakirilyuk@gmail.com>
2022-05-02 20:42:11 -0400

    commit tree 7b5d20e63f823100bfdc02dbcb87f8bc1933de05

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+
+
+function createEmployeeRecord(employee){
+    return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(event){
+
+}
+
+
+
+
+// function createTimeOutEvent
+// function hoursWorkedOnDate
+// function function wagesEarnedOnDate
+// function allWagesFor
+// function calculatePayroll
